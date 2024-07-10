6458805063daa302d7e6b8e0f964fa6d6c2f0d17
Vibhuti_ <vibhutipratibha@gmail.com>
2024-07-10 10:53:39 +0530

    Removed unwanted files

--- Customers-SpringBatch/src/main/java/com/example/springbatch/repository/CustomerRepository.java
+++
@@ -1,7 +0,0 @@
-package com.example.springbatch.repository;
-
-import com.example.springbatch.entity.Customer;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface CustomerRepository  extends JpaRepository<Customer,Integer> {
-}
